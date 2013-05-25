6d859c76150e4a1da92002f39cf85eeceff87fad
Francis Tseng <ftzeng@gmail.com>
2013-05-24 20:51:16 -0400

    cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,6 @@ from app import routes, models
 from apscheduler.scheduler import Scheduler
 import parrott
 
-neg = [example.strip() for example in open("data/neg.txt").readlines()]
-pos = [example.strip() for example in open("data/pos.txt").readlines()]
-
-print "Training on %i positive examples" % len(pos)
-print "Training on %i negative examples" % len(neg)
-
 #parrott = parrott.Parrott( neg, pos )
 
 # Schedule the Collector
