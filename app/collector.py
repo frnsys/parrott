bdd09332f5b5f63bb8cc56b01750ab51033c7135
Francis Tseng <ftzeng@gmail.com>
2013-05-29 18:39:27 -0400

    collector collects tweet text, username, and a list of expanded urls

--- app/collector.py
+++ app/collector.py
@@ -11,31 +11,36 @@ Run as a cron job.
 
 import sys
 from parrott import membrane, memory
+import pprint
 
 def tweets():
-	'''
-	Collects latest 20 timeline
+    '''
+    Collects latest 20 timeline
     tweets for authenticated user.
 
     Returns:
         List of Tweet texts
-	'''
-	twitter = membrane.twitter.api()
+    '''
+    twitter = membrane.twitter.api()
 
-    # Collect tweets and
-    # concatenate tweet text and username
-	tweets = [' '.join([tweet.text, tweet.user.screen_name]).encode('utf-8')
-                for tweet in twitter.home_timeline()]
+    # Collect tweets
+    tweets = []
+    for tweet in twitter.home_timeline():
+        tweets.append({
+            'tweet': tweet.text.encode('utf-8'),
+            'user': tweet.user.screen_name.encode('utf-8'),
+            'links': [link['expanded_url'] for link in tweet.entities['urls']]
+            })
 
-	return tweets
+    return tweets
 
 def user_tweets( user ):
-	'''
-	Collect latest 200 tweets from specified user(name).
-	'''
-	twitter = membrane.twitter.api()
-	tweets = [tweet.text.encode('utf-8') for tweet in twitter.user_timeline(screen_name=user, count=200)]
-	return tweets
+    '''
+    Collect latest 200 tweets from specified user(name).
+    '''
+    twitter = membrane.twitter.api()
+    tweets = [tweet.text.encode('utf-8') for tweet in twitter.user_timeline(screen_name=user, count=200)]
+    return tweets
 
 def collect():
     # Store Tweet text into Solr ("memory")
