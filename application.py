7c82e9f58a968cf58b46cf3ed26fc0213d1b0582
Francis Tseng <ftzeng@gmail.com>
2013-05-29 20:04:37 -0400

    collector is now scheduled in application.py

--- application.py
+++ application.py
@@ -3,18 +3,10 @@
 from app import app, collector
 from apscheduler.scheduler import Scheduler
 
-import datetime
-
-def foo():
-    myFile = open ('/Users/ftseng/Desktop/foo.txt', 'w')
-    myFile.write(str(datetime.datetime.now()))
-    myFile.close()
-
 if __name__ == '__main__':
     # Schedule the Collector
     scheduler = Scheduler()
-    #scheduler.add_interval_job(collector.collect(), minutes=30)
-    scheduler.add_interval_job(foo, seconds=5)
+    scheduler.add_interval_job(collector.collect(), minutes=30)
     scheduler.start()
 
     app.run(debug=True)
