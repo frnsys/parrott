2e5f9efda7f542f9fe31d322549f22e7e22221f4
Francis Tseng <ftzeng@gmail.com>
2013-06-21 19:51:19 -0400

    cleaned up some cold code

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,3 @@ from app import routes
 # Create the Parrott!
 from parrott import Parrott
 app.parrott = Parrott()
-
-# This works!
-tweet_id='544eea61-a583-4ec9-af50-7c40ff47671f'
-# Fetch current tweet
-tweet = app.parrott.memory.recall(tweet_id)
-# If a matching tweet was found, audit as positive
-if tweet:
-    app.parrott.audit(tweet, True) # test auditing
